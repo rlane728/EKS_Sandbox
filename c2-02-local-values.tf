9a1519b4bd07e831b1a4c2655e1923b5862f8e82
rlane728 <rlane728@gmail.com>
2023-09-29 10:32:28 -0500

    Initial Commit

--- c2-02-local-values.tf
+++ c2-02-local-values.tf
@@ -8,5 +8,6 @@ locals {
     owners      = local.owners
     environment = local.environment
   }
-  eks_cluster_name = "${local.name}-${var.cluster_name}"  
+  eks_cluster_name = "EKS_Sandbox" #<-- Setting to EKS_Sandbox for now...
+  #eks_cluster_name = "${local.name}-${var.cluster_name}"  
 } 
