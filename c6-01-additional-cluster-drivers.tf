d33a39ff219049a8b560711dfffea84cd9bccd91
rlane728 <rlane728@gmail.com>
2023-10-01 05:31:42 -0500

    Minor tweaks to nodegroup

--- c6-01-additional-cluster-drivers.tf
+++ c6-01-additional-cluster-drivers.tf
@@ -11,11 +11,12 @@ resource "aws_eks_addon" "aws-ebs-csi-driver" {
 }
 
 # Adding CoreDNS
-# This will allow us to more easily attached prometheus datasources to grafana
+# This will allow us to more easily attach prometheus datasources to grafana
 resource "aws_eks_addon" "aws-coredns-driver" {
   cluster_name                = aws_eks_cluster.eks_cluster.name
   addon_name                  = "coredns"
-  addon_version               = "v1.10.1-eksbuild.4"
+  addon_version               = "v1.10.1-eksbuild.1"
+  #addon_version              = "v1.10.1-eksbuild.4"  #<--- Errors out when installing
   resolve_conflicts_on_update = "PRESERVE"
 
   depends_on = [aws_eks_node_group.eks_ng_public]
